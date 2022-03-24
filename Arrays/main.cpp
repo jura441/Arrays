5390fc088bf745f7f49622df5f8da5fd192edc81
jura441 <102172272+jura441@users.noreply.github.com>
2022-03-24 21:30:49 +0300

    Add project files.

---
+++ Arrays/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5; 
+	int arr[n] = { };
+	//arr[2] = 123;
+	cout << "Введите элементы массива (" << n << " шт):";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+}
+	// вывод массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	// вывод массива на экран в обратном порядке:
+	for (int i = n - 1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	// Сумма элементов массива:
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива: " << sum << endl;
+	cout << "Среднее арифметичское элементов массива: " << sum / n << endl;
+	// минимальное и максимальное значение
+	int min, max;
+	min = max = arr[0]; 
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)min = arr[i];
+	}
+	cout << "Минимальное значение: " << min << endl;
+	cout << "Максимальное значение: " << max << endl;
+
+}
